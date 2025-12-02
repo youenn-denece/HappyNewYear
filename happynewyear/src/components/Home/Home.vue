248eaf4f19e307f79c387a410e9cdaded00630ab
youenn-denece <youenn.denece@gmail.com>
2025-12-02 23:06:44 +0100

    mise en place => sans chargement

---
+++ happynewyear/src/components/Home/Home.vue
@@ -0,0 +1,21 @@
+<template src="Home.html"></template>
+<style src="Home.css"></style>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
+
+export default defineComponent({
+  name: 'Home',
+  setup() {
+    const router = useRouter();
+    let ville = '';
+    let genre = '';
+
+    const goToResult = () => {
+      router.push({ path: '/result', query: { ville, genre } });
+    };
+
+    return { ville, genre, goToResult };
+  }
+});
+</script>
