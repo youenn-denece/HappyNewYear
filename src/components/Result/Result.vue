890f753d17b9d3a0a1a20da0b1f9496e3f036a2d
youenn-denece <youenn.denece@gmail.com>
2025-12-03 00:38:13 +0100

    mise en place

---
+++ src/components/Result/Result.vue
@@ -0,0 +1,22 @@
+<template src="./Result.html"></template>
+<style src="./Result.css"></style>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useRoute } from 'vue-router';
+
+export default defineComponent({
+  name: 'Result',
+  setup() {
+    const route = useRoute();
+    const ville = (route.query.ville as string)?.toLowerCase();
+    const genre = route.query.genre as string;
+
+    let nom = 'Invité';
+    if (ville === 'quimper' && genre === 'homme') {
+      nom = 'Antoine';
+    }
+
+    return { nom };
+  }
+});
+</script>
---
+++ src/router/index.ts
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import type { RouteRecordRaw } from 'vue-router';
+import Home from '../components/Home/Home.vue';
+import Result from '../components/Result/Result.vue';
+
+const routes: Array<RouteRecordRaw> = [
+  { path: '/', name: 'Home', component: Home },
+  { path: '/result', name: 'Result', component: Result }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
