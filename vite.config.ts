e9047017385436e53a07e88b38e168570f979fbb
youenn-denece <youenn.denece@gmail.com>
2026-01-03 11:43:22 +0100

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'HappyNewYear'
+  base: 'HappyNewYear',
+  build: {
+    outDir: 'dist'
+  }
 })
